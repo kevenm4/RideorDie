dfdced36b1eba9fd4c5bab24ed2011e11ca4e9d8
keven andrade <keven111299@gmail.com>
2022-04-28 12:24:25 +0100

    fourth

--- coverage/game.js
+++ coverage/game.js
@@ -153,7 +153,7 @@ class Game {
         this.ctx.fillText(`KM: ${score}`, 100, 30);
     }
     drawBackground() {
-        this.background.src = "/Doc/assets/Image/road_1.png";
+        this.background.src = "./Doc/assets/Image/road_1.png";
         this.ctx.drawImage(
             this.background,
             this.x,
